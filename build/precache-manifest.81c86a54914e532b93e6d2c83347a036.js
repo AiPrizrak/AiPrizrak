44b16b91ddb9ea184f1f6ae2e916be23188b01c0
AiPrizrak <nnwarts@gmail.com>
2021-08-05 04:35:20 +0300

    finished project

---
+++ build/precache-manifest.81c86a54914e532b93e6d2c83347a036.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a024c507fb3d3864fa584f4b3e58b00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ba79fa03faabca4ffa3",
+    "url": "/static/js/2.9240a40d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9240a40d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f452b754fd07697995",
+    "url": "/static/js/main.f6454b0d.chunk.js"
+  },
+  {
+    "revision": "6fc7714c26f26ebd99cb",
+    "url": "/static/js/runtime-main.5dcce831.js"
+  },
+  {
+    "revision": "d29b0fab34ab358e93ca27cc8e74ec77",
+    "url": "/static/media/poster_collage.d29b0fab.jpg"
+  }
+]);
